dbb514f91c2d23c0b6604394a8d7527e5b8b120c
shaaaaneeee <shaneslee18@icloud.com>
2026-03-27 23:41:26 +0800

    Added new Vercel domain for CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:4173",
-        "https://repd.vercel.app",  # update with your real domain later
+        "https://repd-sigma.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
